7e52b427f6858a69c9e5cde5f021b0a6bacf0fca
djogo <stefan.djogo@fintechcube.com>
2023-02-10 14:39:03 +0100

    Initial commit 1

---
+++ src/test/java/guru/springframework/beerservice/BeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.beerservice;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
